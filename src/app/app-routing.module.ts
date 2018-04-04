796dc65bcc2b240aef98e56420fb478aedc32416
xCyrusT <lkthu.vn@gmail.com>
2018-04-04 10:37:15 +0700

    Create shared/models/accout.ts, auth/login/login.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RolesRoutingModule } from './roles/roles-routing.module';
-import { AuthRoutingModule } from './auth/auth-routing.module';
-import { UsersRoutingModule } from './users/users-routing.module';
+
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { AuthModule } from './auth/auth.module';
+
 
 const routes: Routes = [
-  { path: 'users', component: UsersRoutingModule },
-  { path: 'roles', component: RolesRoutingModule },
-  { path: 'auth', component: AuthRoutingModule },
+  { path: 'users', loadChildren: './users/users.module#UsersModule' },
+  { path: 'roles', loadChildren: './roles/roles.module#RolesModule' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
 ];
 
 @NgModule({
