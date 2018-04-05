a28aeac1f2dfad42950fb740595da892aff4e0e4
xCyrusT <lkthu.vn@gmail.com>
2018-04-05 15:49:18 +0700

    Finish Step 04

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthRoutingModule
   ],
-  declarations: []
+  declarations: [LoginComponent, RegisterComponent]
 })
 export class AuthModule { }
--- src/app/roles/roles.module.ts
+++ src/app/roles/roles.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RolesRoutingModule } from './roles-routing.module';
+import { RoleComponent } from './role/role.component';
+import { RoleFormComponent } from './role-form/role-form.component';
+import { RoleListComponent } from './role-list/role-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RolesRoutingModule
   ],
-  declarations: []
+  declarations: [RoleComponent, RoleFormComponent, RoleListComponent]
 })
 export class RolesModule { }
