002301b44fd056f2492c1be4cafd34160a23fd3f
xCyrusT <lkthu.vn@gmail.com>
2018-04-05 16:55:57 +0700

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler/src/core';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'user', loadChildren: './users/users.module#UsersModule', data: { preload: true} },
+  { path: 'role', loadChildren: './roles/roles.module#RolesModule', data: { preload: true} },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'user/create', component: UserFormComponent },
+  { path: 'user/:id', component: UserFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
